1682387b6acfb9cf7cc5e78b3c0411c860b84624
kamal97sh <87917373+kamal97sh@users.noreply.github.com>
2021-12-19 22:24:20 +0200

    Initial commit

---
+++ bin/compare.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(List<String> arguments) {
+
+     String fristName=stdin.readLineSync()!;
+     String secondName=stdin.readLineSync()!;
+ 
+       if(fristName==secondName)
+       {
+         print("Same Words");
+       }
+       else
+        {
+          ("not Same Words");
+        }
+}
